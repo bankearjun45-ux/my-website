03db3ad92cc2caca71ceecba79fea25207e4f670
bankearjun45-ux <bankearjun45@gmail.com>
2026-05-07 00:56:06 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// script.js
+
+// IMAGE SLIDER
+
+const slides = document.querySelectorAll(".slide");
+
+let current = 0;
+
+function changeSlide(){
+
+  slides[current].classList.remove("active");
+
+  current = (current + 1) % slides.length;
+
+  slides[current].classList.add("active");
+
+}
+
+setInterval(changeSlide, 3000);
+
+// SCROLL BUTTON
+
+function scrollToContact(){
+
+  document.getElementById("contact")
+  .scrollIntoView({
+    behavior:"smooth"
+  });
+
+}
